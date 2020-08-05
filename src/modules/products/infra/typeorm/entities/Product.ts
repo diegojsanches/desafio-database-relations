33e03bd2b45647d8defec6fc9beaa103d175c6c7
diegojsanches <diegojsanches@hotmail.com>
2020-08-05 02:14:37 -0300

    Ajuste das entidades e controllers

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -9,19 +9,28 @@ import {
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('products')
 class Product {
+  @Column()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column('decimal')
   price: number;
 
+  @Column('integer')
   quantity: number;
 
+  @OneToMany(() => OrdersProducts, order => order.product)
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
